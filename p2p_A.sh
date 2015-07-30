ae19fd9a51df27840bf19af0d874fa0f965b7f33
gaixas1 <gaixas1@gmail.com>
2015-07-30 09:26:26 +0200

    p2p example ??

---
+++ p2p_A.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# point-to-point || Server A
+# eth1 -> Server B
+INSTALLATION_PATH= "/usr/local/irati"
+
+
+ip link add link eth1 name eth1.100 type vlan id 100
+ip link set dev eth1 up
+ip link set dev eth1.100 up
+
+modprobe shim-eth-vlan
+modprobe rina-default-plugin
+modprobe normal-ipcp
+
+$INSTALLATION_PATH/bin/ipcm -c ./p2p/ipcmanager_A.conf
